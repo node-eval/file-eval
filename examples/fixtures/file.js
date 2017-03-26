b1376c960096d31766eac63ac289a206a5633c24
blond <andrewblond@yandex.com>
2017-03-26 13:04:01 +0300

    chore(examples): add examples

---
+++ examples/fixtures/file.js
@@ -0,0 +1,14 @@
+// This file is written in CommonJS syntax.
+
+module.exports = {
+  name: 'file-eval',
+  description: 'Read file and eval it',
+  license: 'MIT',
+  author: 'Andrew Abramov <mail@blond.im> (github.com/blond)',
+  keywords: [
+    "file",
+    "read",
+    "eval",
+    "require"
+  ]
+};
